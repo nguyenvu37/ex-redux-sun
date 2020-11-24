5977b5909a9d2bc61bd16172abce4a04c1f68dcb
Anh Vũ Nguyễn <nguyenanhvu.bkdn@gmail.com>
2020-11-24 23:03:06 +0700

    Updates

---
+++ precache-manifest.82a44ec5e7957ce93b6ab28fc77137e2.js
@@ -0,0 +1,410 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6059a4093667a92c19c8baa6fc2811a",
+    "url": "/ex-redux-sun/index.html"
+  },
+  {
+    "revision": "210bfa4f8077e4a17e7b",
+    "url": "/ex-redux-sun/static/css/main.4c113c26.chunk.css"
+  },
+  {
+    "revision": "7dc7b1d6c833a403370a",
+    "url": "/ex-redux-sun/static/js/2.6ce0e2f8.chunk.js"
+  },
+  {
+    "revision": "332ed85654f82f2aa8b9",
+    "url": "/ex-redux-sun/static/js/3.0c591c26.chunk.js"
+  },
+  {
+    "revision": "2a1d75cb67e740336df6",
+    "url": "/ex-redux-sun/static/js/4.028c83da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ex-redux-sun/static/js/4.028c83da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210bfa4f8077e4a17e7b",
+    "url": "/ex-redux-sun/static/js/main.fdaa2d94.chunk.js"
+  },
+  {
+    "revision": "d7829b62b967d13ae1e5",
+    "url": "/ex-redux-sun/static/js/runtime-main.489f449a.js"
+  },
+  {
+    "revision": "8bdc2915b0997cc3df1c5de303f42ffc",
+    "url": "/ex-redux-sun/static/media/apple-airpods-white.8bdc2915.jpg"
+  },
+  {
+    "revision": "2f4ca76e1db18e770fc9958690a986c3",
+    "url": "/ex-redux-sun/static/media/beat-headphone-balck.2f4ca76e.jpg"
+  },
+  {
+    "revision": "be206a11c3cf5578747a412ce7170951",
+    "url": "/ex-redux-sun/static/media/beat-headphones-black.be206a11.jpg"
+  },
+  {
+    "revision": "60897ebe858e9ffcf8599559b00e8aad",
+    "url": "/ex-redux-sun/static/media/beat-headphones-black2.60897ebe.jpg"
+  },
+  {
+    "revision": "75d14317fe37844802042dfa94b43b4d",
+    "url": "/ex-redux-sun/static/media/beat-headphones-black3.75d14317.jpg"
+  },
+  {
+    "revision": "6cd4491ed184c90f1a2c09710d5b76b4",
+    "url": "/ex-redux-sun/static/media/beat-headphones-rosegold.6cd4491e.jpg"
+  },
+  {
+    "revision": "95c5bf506aad92e7aad5ad86cefab540",
+    "url": "/ex-redux-sun/static/media/bionaire-fan-black.95c5bf50.jpg"
+  },
+  {
+    "revision": "fa903b61e50612dcedca595d89a81971",
+    "url": "/ex-redux-sun/static/media/bionaire-fan-black2.fa903b61.jpg"
+  },
+  {
+    "revision": "45de4e9ffcc4ee632b04c7fe1aece1df",
+    "url": "/ex-redux-sun/static/media/bosch-dishwasher-black.45de4e9f.jpg"
+  },
+  {
+    "revision": "f615f23c92c03876f0cb11d8a860b614",
+    "url": "/ex-redux-sun/static/media/bosch-dishwasher-blackstainless.f615f23c.jpg"
+  },
+  {
+    "revision": "4536b84311d95ed1f4063e8a4ffb75f6",
+    "url": "/ex-redux-sun/static/media/bosch-dishwasher-silver.4536b843.jpg"
+  },
+  {
+    "revision": "2cd24160b33c03c85870f91d05aac96b",
+    "url": "/ex-redux-sun/static/media/bose-headphones-black.2cd24160.jpg"
+  },
+  {
+    "revision": "f535f8d7caab0ea9ee34ede6d5bf6427",
+    "url": "/ex-redux-sun/static/media/bose-headphones-gray.f535f8d7.jpg"
+  },
+  {
+    "revision": "37187a0ac61a27badf579c8b360fc6fd",
+    "url": "/ex-redux-sun/static/media/dyson-fan-black.37187a0a.jpg"
+  },
+  {
+    "revision": "04e346c69699d0a52978e16208f430f1",
+    "url": "/ex-redux-sun/static/media/dyson-fan-blue.04e346c6.jpg"
+  },
+  {
+    "revision": "355939357ca56e80de9e298954675092",
+    "url": "/ex-redux-sun/static/media/dyson-fan-blue2.35593935.jpg"
+  },
+  {
+    "revision": "12228e3194826af56756bab0dee58ccd",
+    "url": "/ex-redux-sun/static/media/dyson-fan-white.12228e31.jpg"
+  },
+  {
+    "revision": "518072f561d8a6f0687137cbb08ccac2",
+    "url": "/ex-redux-sun/static/media/febreze-fan-black.518072f5.jpg"
+  },
+  {
+    "revision": "7f3edd23dd5730982b16f382788e6e8e",
+    "url": "/ex-redux-sun/static/media/frigidaire-dishwasher-stainless.7f3edd23.jpg"
+  },
+  {
+    "revision": "b016c4dd26668327de307ced54db10fe",
+    "url": "/ex-redux-sun/static/media/harman-speaker-gray.b016c4dd.jpg"
+  },
+  {
+    "revision": "575996e8a92a26f00365a8a9da08d6f3",
+    "url": "/ex-redux-sun/static/media/harman-wireless-black.575996e8.jpg"
+  },
+  {
+    "revision": "bfc17e4c8b4ea2c9a41e34348bc86799",
+    "url": "/ex-redux-sun/static/media/holmes-fan-black.bfc17e4c.jpg"
+  },
+  {
+    "revision": "4d8bb4999485d2f6eda7cd76a753e423",
+    "url": "/ex-redux-sun/static/media/holmes-fan-white.4d8bb499.jpg"
+  },
+  {
+    "revision": "1e134cd40a1ea511a69488f38d45d61d",
+    "url": "/ex-redux-sun/static/media/honeywell-fan-black.1e134cd4.jpg"
+  },
+  {
+    "revision": "90f67eca6e5537471304718c9dc2b549",
+    "url": "/ex-redux-sun/static/media/honeywell-fan-black2.90f67eca.jpg"
+  },
+  {
+    "revision": "d0336084ab399e8292bd75cb42d9a8d9",
+    "url": "/ex-redux-sun/static/media/honeywell-fan-black3.d0336084.jpg"
+  },
+  {
+    "revision": "0f2478ec325b01e8712017731410340b",
+    "url": "/ex-redux-sun/static/media/honeywell-fan-black4.0f2478ec.jpg"
+  },
+  {
+    "revision": "9c60ba4b1e50c352db451199a154940f",
+    "url": "/ex-redux-sun/static/media/insignia-fan-black.9c60ba4b.jpg"
+  },
+  {
+    "revision": "091701f5938db1d421b49e4d32696755",
+    "url": "/ex-redux-sun/static/media/insignia-fan-blue.091701f5.jpg"
+  },
+  {
+    "revision": "9b6234d74b7899c2e41f122b2434d695",
+    "url": "/ex-redux-sun/static/media/insignia-fan-blue2.9b6234d7.jpg"
+  },
+  {
+    "revision": "6dfeccefbafdaad488141c02c0de0eaa",
+    "url": "/ex-redux-sun/static/media/insignia-fan-caviarblack.6dfeccef.jpg"
+  },
+  {
+    "revision": "126a189f44cbcff3805077aa427aaa2e",
+    "url": "/ex-redux-sun/static/media/insignia-fan-copper.126a189f.jpg"
+  },
+  {
+    "revision": "d0e473244a070f76c10cd8fde3706f27",
+    "url": "/ex-redux-sun/static/media/insignia-fan-gray.d0e47324.jpg"
+  },
+  {
+    "revision": "0aea3f4102a8f982fc25b044a31a263d",
+    "url": "/ex-redux-sun/static/media/insignia-fan-gunmetal.0aea3f41.jpg"
+  },
+  {
+    "revision": "c5d75ba222beb71659cbafce80589a1f",
+    "url": "/ex-redux-sun/static/media/insignia-fan-mint.c5d75ba2.jpg"
+  },
+  {
+    "revision": "64bb86cad0a259a9c03bc8759f658c2e",
+    "url": "/ex-redux-sun/static/media/insignia-fan-orange.64bb86ca.jpg"
+  },
+  {
+    "revision": "050188127b397d80069666bcf5b16ec3",
+    "url": "/ex-redux-sun/static/media/insignia-fan-pink.05018812.jpg"
+  },
+  {
+    "revision": "d5a7fb4e18ab63baaa902a75713104fc",
+    "url": "/ex-redux-sun/static/media/insignia-fan-stainless.d5a7fb4e.jpg"
+  },
+  {
+    "revision": "d45be37f59c5aed637b102b07242a92c",
+    "url": "/ex-redux-sun/static/media/insignia-fan-truered.d45be37f.jpg"
+  },
+  {
+    "revision": "8795f88fcc2ccb76a40bd51f9d09c446",
+    "url": "/ex-redux-sun/static/media/insignia-headphones-white.8795f88f.jpg"
+  },
+  {
+    "revision": "f27cd74e4213efbb4ad2d6ca88151554",
+    "url": "/ex-redux-sun/static/media/insignia-homeaudio-black2.f27cd74e.jpg"
+  },
+  {
+    "revision": "a6490ef2140da1b254d5449948cff36f",
+    "url": "/ex-redux-sun/static/media/irobot-black.a6490ef2.jpg"
+  },
+  {
+    "revision": "60537ecf1ab7767384c1c63a1485f165",
+    "url": "/ex-redux-sun/static/media/jaybird-headphones-blackout.60537ecf.jpg"
+  },
+  {
+    "revision": "3aa4ac3840b2ce5707d9d6f09711b5d5",
+    "url": "/ex-redux-sun/static/media/jvc-headphones-black.3aa4ac38.jpg"
+  },
+  {
+    "revision": "d94ed473c0f09b9ea33c2836fd6b7c1c",
+    "url": "/ex-redux-sun/static/media/jvc-headphones-black2.d94ed473.jpg"
+  },
+  {
+    "revision": "6b6cea83586edbc8fb76586268477a26",
+    "url": "/ex-redux-sun/static/media/kitchenaid-dishwasher-black.6b6cea83.jpg"
+  },
+  {
+    "revision": "a08f592978a6f77d8e9131dd38c3fa7b",
+    "url": "/ex-redux-sun/static/media/kitchenaid-dishwasher-gray.a08f5929.jpg"
+  },
+  {
+    "revision": "2c7b4953165c6dd1a4e82a6651db9e67",
+    "url": "/ex-redux-sun/static/media/kitchenaid-dishwasher-stainless.2c7b4953.jpg"
+  },
+  {
+    "revision": "425a148c0dcbbeef5dcbe56cf456fb05",
+    "url": "/ex-redux-sun/static/media/lasko-fan-black.425a148c.jpg"
+  },
+  {
+    "revision": "851a48e19b6b63fb403fb3f20c8dc4d8",
+    "url": "/ex-redux-sun/static/media/lasko-fan-white.851a48e1.jpg"
+  },
+  {
+    "revision": "adc90e85e340eb48785588d001433159",
+    "url": "/ex-redux-sun/static/media/lg-dishwasher-gold.adc90e85.jpg"
+  },
+  {
+    "revision": "ce4dc956bb38ad0197117935d9812b81",
+    "url": "/ex-redux-sun/static/media/lg-dishwasher-gray.ce4dc956.jpg"
+  },
+  {
+    "revision": "dc9c8765a24d3ec291ff0afd414d1627",
+    "url": "/ex-redux-sun/static/media/lg-headphone-black.dc9c8765.jpg"
+  },
+  {
+    "revision": "c01ff5b4b2cc91cab0d8aa1a5ea36e85",
+    "url": "/ex-redux-sun/static/media/lg-headphones-black.c01ff5b4.jpg"
+  },
+  {
+    "revision": "dc9c8765a24d3ec291ff0afd414d1627",
+    "url": "/ex-redux-sun/static/media/lg-headset-black.dc9c8765.jpg"
+  },
+  {
+    "revision": "b435f9f0b707c541559c808acf10ef11",
+    "url": "/ex-redux-sun/static/media/maytag-dishwasher-gray.b435f9f0.jpg"
+  },
+  {
+    "revision": "979e2a1cfe1e8d147000fb51fed6b24d",
+    "url": "/ex-redux-sun/static/media/maytag-dishwasher-silver.979e2a1c.jpg"
+  },
+  {
+    "revision": "895d341705e6558bd6474f569d6a066b",
+    "url": "/ex-redux-sun/static/media/polk-homeaudio-black.895d3417.jpg"
+  },
+  {
+    "revision": "cb243051d304096eb8820ac53133c31b",
+    "url": "/ex-redux-sun/static/media/rose-headphones-silver.cb243051.jpg"
+  },
+  {
+    "revision": "2a9038363d42c637f14584eb0da021e9",
+    "url": "/ex-redux-sun/static/media/rose-homeaudio-black.2a903836.jpg"
+  },
+  {
+    "revision": "46c424c04a54b45977dee266398ba82d",
+    "url": "/ex-redux-sun/static/media/rose-homeaudio-black2.46c424c0.jpg"
+  },
+  {
+    "revision": "6006946ce9232aecad850b7edb7e839c",
+    "url": "/ex-redux-sun/static/media/rose-homeaudio-black3.6006946c.jpg"
+  },
+  {
+    "revision": "e39e776f1937854e84b511c301232698",
+    "url": "/ex-redux-sun/static/media/samsung-dishwasher-black.e39e776f.jpg"
+  },
+  {
+    "revision": "5ed338570df6a3749962ce6d77669832",
+    "url": "/ex-redux-sun/static/media/samsung-dishwasher-blackstainless.5ed33857.jpg"
+  },
+  {
+    "revision": "577b87a30f45d56412fa741a7252ee3d",
+    "url": "/ex-redux-sun/static/media/samsung-dishwasher-gold.577b87a3.jpg"
+  },
+  {
+    "revision": "7fe2aced7843a4c09b89bb7ce05ddc4e",
+    "url": "/ex-redux-sun/static/media/samsung-dishwasher-gray.7fe2aced.jpg"
+  },
+  {
+    "revision": "a868fa486730847a9630543d6f3184bc",
+    "url": "/ex-redux-sun/static/media/samsung-dishwasher-silver.a868fa48.jpg"
+  },
+  {
+    "revision": "44e935087d55017ef880f9e85d2440f4",
+    "url": "/ex-redux-sun/static/media/samsung-dishwasher-stainlesssteel.44e93508.jpg"
+  },
+  {
+    "revision": "5469a66df6482ea4558d44878a671df6",
+    "url": "/ex-redux-sun/static/media/samsung-headphones-black.5469a66d.jpg"
+  },
+  {
+    "revision": "a262c88fb400eea45d3e563adfd30e76",
+    "url": "/ex-redux-sun/static/media/samsung-homeaudio-black.a262c88f.jpg"
+  },
+  {
+    "revision": "8a642260c528ec278f55e322a50cadd7",
+    "url": "/ex-redux-sun/static/media/samsung-homeaudio-black2.8a642260.jpg"
+  },
+  {
+    "revision": "7f9c6608d5b25b60677620ba34a8c768",
+    "url": "/ex-redux-sun/static/media/samsung-homeaudio-black3.7f9c6608.jpg"
+  },
+  {
+    "revision": "888b4f519cce7909de4f152a951c62cc",
+    "url": "/ex-redux-sun/static/media/samsung-homeaudio-black4.888b4f51.jpg"
+  },
+  {
+    "revision": "c97a8d1e29af9f191715f17d72fcd5c7",
+    "url": "/ex-redux-sun/static/media/skullcandy-headdphones-black.c97a8d1e.jpg"
+  },
+  {
+    "revision": "499ebc6899807080db29cf63b9b781df",
+    "url": "/ex-redux-sun/static/media/skullcandy-headphones-blue.499ebc68.jpg"
+  },
+  {
+    "revision": "480d3fca431ce318ea7ed339f5b954f1",
+    "url": "/ex-redux-sun/static/media/sonos-homeaudio-black.480d3fca.jpg"
+  },
+  {
+    "revision": "d8200e0175a2837e8960aeea33ba7c47",
+    "url": "/ex-redux-sun/static/media/sonos-homeaudio-black2.d8200e01.jpg"
+  },
+  {
+    "revision": "b6d2d4df200098d736f6e006bb58d011",
+    "url": "/ex-redux-sun/static/media/sonos-homeaudio-white.b6d2d4df.jpg"
+  },
+  {
+    "revision": "f143bc23459f1ab8ba7a0a83b29dcbf7",
+    "url": "/ex-redux-sun/static/media/sony-headphones-black2.f143bc23.jpg"
+  },
+  {
+    "revision": "e8428b1490e75c99cb94b205a9b94e84",
+    "url": "/ex-redux-sun/static/media/sony-headphones-black3.e8428b14.jpg"
+  },
+  {
+    "revision": "a9a23410677b79d8108bc6ef4e9a9780",
+    "url": "/ex-redux-sun/static/media/sony-headphones-black4.a9a23410.jpg"
+  },
+  {
+    "revision": "c9606123e6fa234644c3f0a552edc3c1",
+    "url": "/ex-redux-sun/static/media/sony-headphones-black5.c9606123.jpg"
+  },
+  {
+    "revision": "dd84cfae756db53adb0c007e0796fc95",
+    "url": "/ex-redux-sun/static/media/sony-headphones-white.dd84cfae.jpg"
+  },
+  {
+    "revision": "7b120ce06237cd47a6b7d37abc15c9e8",
+    "url": "/ex-redux-sun/static/media/sony-homeaudio-black.7b120ce0.jpg"
+  },
+  {
+    "revision": "e1da619e21f2a07e22c5edd09d805ed3",
+    "url": "/ex-redux-sun/static/media/vizio-homeaudio-balck.e1da619e.jpg"
+  },
+  {
+    "revision": "523a914c6589c4ceb80a8e60985c5944",
+    "url": "/ex-redux-sun/static/media/vornado-fan-black.523a914c.jpg"
+  },
+  {
+    "revision": "3f36992c4f2a65d0e1fd9cb73953b654",
+    "url": "/ex-redux-sun/static/media/vornado-fan-black2.3f36992c.jpg"
+  },
+  {
+    "revision": "f012cd27748228c067c47a87e4f1639b",
+    "url": "/ex-redux-sun/static/media/vornado-fan-pink.f012cd27.jpg"
+  },
+  {
+    "revision": "a75e85748f19bfdb37aa1fb7975fb17c",
+    "url": "/ex-redux-sun/static/media/whirlpool-dishwasher-black.a75e8574.jpg"
+  },
+  {
+    "revision": "ad75d76a73e4c53be38292a5027adfe2",
+    "url": "/ex-redux-sun/static/media/whirlpool-dishwasher-gold.ad75d76a.jpg"
+  },
+  {
+    "revision": "e141030e7922812cbce2f62ef52ecc03",
+    "url": "/ex-redux-sun/static/media/whirlpool-dishwasher-silver.e141030e.jpg"
+  },
+  {
+    "revision": "70db6a797b66df2aaeda308ec9b3058f",
+    "url": "/ex-redux-sun/static/media/whirlpool-dishwasher-stainless.70db6a79.jpg"
+  },
+  {
+    "revision": "701c6dda17b793019604b819ab01f982",
+    "url": "/ex-redux-sun/static/media/whirlpool-dishwasher-white.701c6dda.jpg"
+  },
+  {
+    "revision": "46ceece7daec4231212d1b1a7ce8a4ea",
+    "url": "/ex-redux-sun/static/media/yamaha-homeaudio-black.46ceece7.jpg"
+  }
+]);
